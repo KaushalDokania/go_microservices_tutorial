4b8100a3e19802dc0e76b4ba7ec909ba73de9489
Kaushal Dokania <kaushal.dokania@microfocus.com>
2021-09-30 00:05:21 +0530

    Added timeouts & graceful shutdown with os signals

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/KaushalDokania/go_microservices_tutorial/handlers"
 )
@@ -17,7 +20,31 @@ func main() {
 	sm.Handle("/", helloHander)
 	sm.Handle("/bye", goodByeHandler)
 
-	http.ListenAndServe(":8080", sm)
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  10 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	logger.Println("Received Terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+	// http.ListenAndServe(":8080", sm)
 }
 
 // Client request: curl -d "some data" localhost:8080
